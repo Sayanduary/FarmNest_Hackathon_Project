090c7af6a472b74dace0b57d56eef4156f0b95f1
Sayanduary <sayanduary@gmail.com>
2025-04-07 10:22:59 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Outlet, useLocation } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer'
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import fetchUserDetails from './utils/fetchUserDetails';
+import { setUserDetails } from './store/userSlice';
+import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import { useDispatch } from 'react-redux';
+import Axios from './utils/Axios';
+import SummaryApi from './common/SummaryApi';
+import { handleAddItemCart } from './store/cartProduct'
+import GlobalProvider from './provider/GlobalProvider';
+import { FaCartShopping } from "react-icons/fa6";
+import CartMobileLink from './components/CartMobile';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const dispatch = useDispatch()
+  const location = useLocation()
+  
+
+  const fetchUser = async()=>{
+      const userData = await fetchUserDetails()
+      dispatch(setUserDetails(userData.data))
+  }
+
+  const fetchCategory = async()=>{
+    try {
+        dispatch(setLoadingCategory(true))
+        const response = await Axios({
+            ...SummaryApi.getCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        
+    }finally{
+      dispatch(setLoadingCategory(false))
+    }
+  }
+
+  const fetchSubCategory = async()=>{
+    try {
+        const response = await Axios({
+            ...SummaryApi.getSubCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        
+    }finally{
+    }
+  }
+
+  
+
+  useEffect(()=>{
+    fetchUser()
+    fetchCategory()
+    fetchSubCategory()
+    // fetchCartItem()
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <GlobalProvider> 
+      <Header/>
+      <main className='min-h-[78vh]'>
+          <Outlet/>
+      </main>
+      <Footer/>
+      <Toaster/>
+      {
+        location.pathname !== '/checkout' && (
+          <CartMobileLink/>
+        )
+      }
+    </GlobalProvider>
+  )
 }
 
-export default App;
+export default App
